f720cb4a0f19cddeac18c8ccff44c58b62333f80
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2023-03-27 20:56:10 +0200

    init

--- helloworld/src/app/app-routing.module.ts
+++ helloworld/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path:'test',component:TestComponent},
-  {path:'login',component:LoginComponent}
+  {path:'',component:LoginComponent}
 ];
 
 @NgModule({
--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { LoginComponent } from './login/login.component';
+import {ButtonModule} from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    MenubarModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
